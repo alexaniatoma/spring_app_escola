2ac5b795b66b03da52e5b68942be0410f89a1756
alexaniatoma <alexaniatoma@hotmail.com>
2025-05-15 10:31:08 -0300

    criando o metodo consultar por id

--- src/main/java/com/alexania/app/escola/controller/ProfessorController.java
+++ src/main/java/com/alexania/app/escola/controller/ProfessorController.java
@@ -3,9 +3,11 @@ package com.alexania.app.escola.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,13 @@ public class ProfessorController {
 	public ResponseEntity<List<Professor>> getAll(){
 		return ResponseEntity.ok(professorRepository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Professor> getById(@PathVariable Long id){
+		return professorRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
 
 }
